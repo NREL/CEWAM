0cde411a2cb66a87e532e49e8ddcbe8e45d35de3
jwalzberg <julien.walzberg@nrel.gov>
2021-07-03 14:09:53 -0600

    Updated sobol

--- Wind_ABM_BatchRun.py
+++ Wind_ABM_BatchRun.py
@@ -32,6 +32,7 @@ if __name__ == '__main__':
         "calibration_6": 1,
         "calibration_7": 1,
         "calibration_8": 1,
+        "calibration_9": 1,
         "manufacturers": {"wind_blade": 5, "plastics_n_boards": 100,
                           "cement": 97},
         "developers": {'lifetime_extension': 10},
@@ -258,12 +259,12 @@ if __name__ == '__main__':
                 "seed": list(range(number_run)),
                 "calibration": [2],
                 "calibration_2": [1E-6, 1],
-                "calibration_3": [0],  # -0.15
+                "calibration_3": [0, -0.7, -0.15],  # -0.15
                 "calibration_4": [-0.26],  # -0.26
-                "calibration_5": [0],  # 0.19
-                "calibration_6": [0],  # 0.29
+                "calibration_5": [0, 0.1, 0.19],  # 0.19
+                "calibration_6": [0.29],  # 0.29
                 "calibration_7": [-0.29],  # -0.29
-                "calibration_8": [0]
+                "calibration_8": [0.17]
             }  # 0.17
             fixed_params = all_fixed_params.copy()
             for key in variable_params.keys():
@@ -282,19 +283,23 @@ if __name__ == '__main__':
             run_data.to_csv("results\\BatchRun.csv")
         else:
             list_variables = [
-                "calibration_2", "calibration_3", "calibration_4"]
-            problem = {'num_vars': 3,
+                "calibration_2", "calibration_3", "calibration_4",
+                "calibration_5", "calibration_6", "calibration_7",
+                "calibration_8", "calibration_9"]
+            problem = {'num_vars': 8,
                        'names': [
-                           "att_bt_mean", "att_bt_man_mean",
-                           "dissolution_rev"],
-                       'bounds': [[0, 1], [0, 1], [0, 2080]]}
-            x = saltelli.sample(problem, 35)
-            baseline_row = np.array([0.8, 0.9, 658])
+                           "shredding_cost", "transport_cost",
+                           "w_a", "w_pbc", "w_dpbc", "w_sn", "w_b", "w_p"],
+                       'bounds': [[0, 132], [0, 0.53], [0, 1], [0, 1], [0, 1],
+                                  [0, 1], [0, 1], [0, 1]]}
+            x = saltelli.sample(problem, 20)
+            baseline_row = np.array([27.56, 0.53, 1, 1, 1, 1, 1, 1])
             x = np.vstack((x, baseline_row))
-            lower_bound_row = np.array([0, 0, 0])
-            x = np.vstack((x, lower_bound_row))
-            upper_bound_row = np.array([1, 1, 2080])
-            x = np.vstack((x, upper_bound_row))
+            # lower_bound_row = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+            # x = np.vstack((x, lower_bound_row))
+            # upper_bound_row = np.array([132, 0.53, 0.53, 0.29, -0.26, -0.29,
+            #                            0.19, -0.15, 0.17])
+            # x = np.vstack((x, upper_bound_row))
             for x_i in range(x.shape[1]):
                 lower_bound = deepcopy(baseline_row)
                 bounds = problem['bounds'][x_i]
@@ -327,7 +332,7 @@ if __name__ == '__main__':
                     'pre_simulation': 2000, 'simulation_start': 2020,
                     'simulation_end': (2020 + number_steps)}
                 # fixed_params["batch_run"] = False
-                fixed_params["calibration"] = 6
+                fixed_params["calibration"] = 5
                 fixed_params.pop("seed")
                 batch_run = set_up_batch_run(
                     nr_processes, variable_params, fixed_params, number_steps)
@@ -340,7 +345,7 @@ if __name__ == '__main__':
             appended_data['x_2'] = appended_data['x_2'].round()
             appended_data.to_csv("results\\SobolBatchRun.csv")
 
-    run_batch(sobol=False, number_steps=31, number_run=5, num_core=6)
+    run_batch(sobol=True, number_steps=31, number_run=5, num_core=6)
 
     t1 = time.time()
     print(t1 - t0)
--- Wind_ABM_Model.py
+++ Wind_ABM_Model.py
@@ -73,6 +73,7 @@ class WindABM(Model):
                  calibration_6=1,
                  calibration_7=1,
                  calibration_8=1,
+                 calibration_9=1,
                  manufacturers={
                      "wind_blade": 5, "plastics_n_boards": 100, "cement": 97},
                  developers={'lifetime_extension': 10},
@@ -459,25 +460,27 @@ class WindABM(Model):
                     'transport_shreds'
                 eol_pathways_transport_mode['landfill'] = 'transport_segments'
                 # shred_cost_copy = transport_shreds['shredding_costs'][1]
-                # transport_shreds['shredding_costs'] = [
-                #    self.calibration_2, 1E-6 + self.calibration_2]
-                # transport_shreds['transport_cost_shreds'] = [
-                #    self.calibration_3, 1E-6 + self.calibration_3]
-                if calibration_4 == 1:
-                    # red_cost = shred_cost_copy - \
-                    #           transport_shreds['shredding_costs'][0]
-                    # for process, cost in rec_processes_costs.items():
-                    #    reduced_costs = [
-                    #        x - red_cost for x in cost]
-                    #    reduced_costs.sort()
-                    #    rec_processes_costs[process] = reduced_costs
-                    recyclers['mechanical_recycling'] = 7
-                    recyclers_states['mechanical_recycling'].extend(
-                        ['Oregon', 'Utah', 'Pennsylvania', 'Nebraska'])
-                tpb_eol_coeff['w_a'] *= calibration_5
-                tpb_eol_coeff['w_sn'] *= calibration_5
-                tpb_eol_coeff['w_b'] *= calibration_5
-                tpb_eol_coeff['w_p'] *= calibration_5
+                transport_shreds['shredding_costs'] = [
+                    calibration_2, 1E-6 + calibration_2]
+                transport_shreds['transport_cost_shreds'] = [
+                    calibration_3, 1E-6 + calibration_3]
+                # if calibration_4 == 1:
+                # red_cost = shred_cost_copy - \
+                #           transport_shreds['shredding_costs'][0]
+                # for process, cost in rec_processes_costs.items():
+                #    reduced_costs = [
+                #        x - red_cost for x in cost]
+                #    reduced_costs.sort()
+                #    rec_processes_costs[process] = reduced_costs
+                # recyclers['mechanical_recycling'] = 7
+                # recyclers_states['mechanical_recycling'].extend(
+                #    ['Oregon', 'Utah', 'Pennsylvania', 'Nebraska'])
+                tpb_eol_coeff['w_a'] *= calibration_4
+                tpb_eol_coeff['w_pbc'] *= calibration_5
+                tpb_eol_coeff['w_dpbc'] = calibration_6
+                tpb_eol_coeff['w_sn'] *= calibration_7
+                tpb_eol_coeff['w_b'] *= calibration_8
+                tpb_eol_coeff['w_p'] *= calibration_9
             elif calibration == 6:
                 attitude_bt_parameters['mean'] = calibration_2
                 attitude_bt_man_parameters['mean'] = calibration_3
@@ -1497,15 +1500,44 @@ class WindABM(Model):
         enacted regulation(s) for the given choice and False otherwise
         :return: the two behaviors with the highest scores
         """
-        scores_sn = self.subjective_norms(grid, adopted_choice, position,
-                                          dic_choices)
-        scores_a = self.attitude(ce_att_level, conv_att_level, dic_choices,
-                                 choices_circularity)
-        scores_pbc = self.perceived_behavioral_control_and_barrier(
-            cost_choices)
-        scores_b = self.perceived_behavioral_control_and_barrier(
-            barrier_choices)
-        scores_p = self.pressure(state, regulations, choices_circularity)
+        scores_sn = self.divide_value_by_tot_dic(
+            self.subjective_norms(grid, adopted_choice, position, dic_choices))
+        scores_a = self.divide_value_by_tot_dic(
+            self.attitude(ce_att_level, conv_att_level, dic_choices,
+                          choices_circularity))
+        scores_pbc = self.divide_value_by_tot_dic(
+            self.perceived_behavioral_control_and_barrier(cost_choices))
+        scores_b = self.divide_value_by_tot_dic(
+            self.perceived_behavioral_control_and_barrier(barrier_choices))
+        scores_p = self.divide_value_by_tot_dic(
+            self.pressure(state, regulations, choices_circularity))
+        scores_behaviors = self.total_tpb_scores(
+            dic_choices, tpb_weights, scores_sn, scores_a, scores_pbc,
+            scores_b, scores_p)
+        first_choice = self.select_highest_scores_in_dic(
+            scores_behaviors, np.nan, True)
+        dic_second_choices = dic_choices.copy()
+        dic_second_choices.pop(first_choice)
+        scores_sn.pop(first_choice)
+        scores_sn = self.divide_value_by_tot_dic(scores_sn)
+        scores_a.pop(first_choice)
+        scores_a = self.divide_value_by_tot_dic(scores_a)
+        scores_pbc.pop(first_choice)
+        scores_pbc = self.divide_value_by_tot_dic(scores_pbc)
+        scores_b.pop(first_choice)
+        scores_b = self.divide_value_by_tot_dic(scores_b)
+        scores_p.pop(first_choice)
+        scores_p = self.divide_value_by_tot_dic(scores_p)
+        scores_behaviors = self.total_tpb_scores(
+            dic_second_choices, tpb_weights, scores_sn, scores_a, scores_pbc,
+            scores_b, scores_p)
+        second_choice = self.select_highest_scores_in_dic(
+            scores_behaviors, first_choice, False)
+        return first_choice, second_choice
+
+    @staticmethod
+    def total_tpb_scores(dic_choices, tpb_weights, scores_sn, scores_a,
+                         scores_pbc, scores_b, scores_p):
         scores_behaviors = {}
         for key, value in dic_choices.items():
             scores_behaviors[key] = tpb_weights['w_bi'] * (
@@ -1517,20 +1549,19 @@ class WindABM(Model):
                     tpb_weights['w_p'] * scores_p[key]
             if not value:
                 scores_behaviors.pop(key)
-        first_choices = [keys for keys, values in scores_behaviors.items() if
-                         values == max(scores_behaviors.values())]
+        return scores_behaviors
+
+    @staticmethod
+    def select_highest_scores_in_dic(dic, first_choice, first):
+        choices = [keys for keys, values in dic.items() if
+                   values == max(dic.values())]
         # if two behavior equally score the highest, choose randomly
-        random.shuffle(first_choices)
-        first_choice = first_choices[0]
-        scores_behaviors.pop(first_choice)
-        second_choices = [keys for keys, values in scores_behaviors.items() if
-                          values == max(scores_behaviors.values())]
-        random.shuffle(second_choices)
-        if second_choices:
-            second_choice = second_choices[0]
+        random.shuffle(choices)
+        if choices or first:
+            selected_choice = choices[0]
         else:
-            second_choice = first_choice
-        return first_choice, second_choice
+            selected_choice = first_choice
+        return selected_choice
 
     @staticmethod
     def lifetime_extension(eol_pathway, initial_lifetime, le_feas_years):
@@ -2090,6 +2121,13 @@ class WindABM(Model):
         weighted_variables = [x * y for x, y in zip(weights, list_variables)]
         weighted_average = sum(weighted_variables)
         return weighted_average
+
+    @staticmethod
+    def divide_value_by_tot_dic(dic):
+        tot = np.nansum(list(dic.values()))
+        if tot != 0:
+            dic = {k: v / tot for k, v in dic.items()}
+        return dic
 
     @staticmethod
     def compute_yearly_or_cum_adoption_ratios(
